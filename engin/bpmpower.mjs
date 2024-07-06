fd12ab49a2e2d4dd279376ac66bf1192230f514b
sayjun0505 <sayjun0505@gmail.com>
2024-07-06 02:37:38 -0700

    a

--- engin/bpmpower.mjs
+++ engin/bpmpower.mjs
@@ -100,14 +100,14 @@ const bpmpowerData = async (io) => {
       total += products.length;
       arr.push(...products);
       if (products.length < pagecount) {
-        const currentDate = new Date();
+        let currentDate = new Date();
         const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, "0");
-        const day = String(currentDate.getDate()).padStart(2, "0");
-        const hours = String(currentDate.getHours()).padStart(2, "0");
-        const minutes = String(currentDate.getMinutes()).padStart(2, "0");
-        const seconds = String(currentDate.getSeconds()).padStart(2, "0");
-        const formattedDateTime = `${year}/${month}/${day} - ${hours}:${minutes}:${seconds}`;
+        let month = String(currentDate.getMonth() + 1).padStart(2, "0");
+        let day = String(currentDate.getDate()).padStart(2, "0");
+        let hours = String(currentDate.getHours()).padStart(2, "0");
+        let minutes = String(currentDate.getMinutes()).padStart(2, "0");
+        let seconds = String(currentDate.getSeconds()).padStart(2, "0");
+        let formattedDateTime = `${year}/${month}/${day} - ${hours}:${minutes}:${seconds}`;
         insertDB(arr, formattedDateTime).then((res) => {
           console.log("start broadcast");
           io.emit("pcbuilder_bpm", formattedDateTime);
