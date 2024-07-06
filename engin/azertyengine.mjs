2699adb1a0cf506751c4a1d2a90df5bdab887f38
sayjun0505 <sayjun0505@gmail.com>
2024-07-06 08:52:59 -0700

    a

--- engin/azertyengine.mjs
+++ engin/azertyengine.mjs
@@ -141,7 +141,7 @@ const parseProductDetails = async (url) => {
       processfamily: processfamily,
       imgURL: imgURL
     };
-    console.log(x)
+    // console.log(x)
     arr.push(x);
   });
   return liCount;
