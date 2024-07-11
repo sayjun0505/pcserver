b038ba056564e837162abb7b770ffd4baae7284c
sayjun0505 <sayjun0505@gmail.com>
2024-07-11 06:52:47 -0700

    a

--- actions/dboperation.mjs
+++ actions/dboperation.mjs
@@ -4,8 +4,11 @@ import CPUVendor from "../model/cpuvendor.js";
 import CPUInfo from "../model/cpuinfo.js";
 import CPUVendorList from "../model/cpuvendorlist.js";
 import CPUNat from "../model/cpunat.js";
-import mongoose from "mongoose";
 import CPUList from "../model/cpulist.js";
+import MboardvendorList from "../model/mboardvendorlist.js";
+import MboardNat from "../model/mboardnat.js";
+import MboardList from "../model/mboardlist.js";
+import mongoose from "mongoose";
 const operRouter = express.Router();
 
 operRouter.get("/api/alldata", async (req, res) => {
@@ -40,5 +43,39 @@ operRouter.get("/api/spec", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
+operRouter.get("/api/allmdata", async (req, res) => {
+  try {
+    const allData = await MboardList.find({});
+    const ret = { data: allData };
+    res.json(ret);
+  } catch (error) {
+    console.error("Error fetching all data from MongoDB:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+operRouter.get("/api/mspec", async (req, res) => {
+  try {
+    const id = req.query.id; 
+    if (id) {
+      const allData = await MboardList.findOne({ _id: id });
+      const vendorData = await MboardvendorList.find({ cpuid: id });
+      const cpunatData = await MboardNat.findOne({ cpuid: id });
+      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      res.json(ret);
+    } else {
+      const allData = await MboardList.find({});
+      const vendorData = await MboardvendorList.find({});
+      const cpunatData = await MboardNat.find({ cpuid: id });
+      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      res.json(ret);
+    }
+  } catch (error) {
+    console.error("Error fetching data from MongoDB:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 
 export default operRouter;
