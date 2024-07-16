add87b5667a9e8b0e203872809c1c437078477e0
sayjun0505 <sayjun0505@gmail.com>
2024-07-15 20:59:28 -0700

    a

--- engin/fixing.mjs
+++ engin/fixing.mjs
@@ -219,10 +219,10 @@ async function fetchCPU() {
     .forBrowser("chrome")
     .setChromeOptions(chromeOptions)
     .build();
-  let pages =10;
+  let pages =25;
   let count = 15;
   try {
-    while (pages<=39) {  
+    while (true) {  
       const url = `https://www.idealo.it/cat/3019I16-${
         count * pages
       }/processori-cpu.html`;
