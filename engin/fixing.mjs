f6065911791287349c5120b22a627e20f44af6ad
sayjun0505 <sayjun0505@gmail.com>
2024-07-16 16:41:48 -0700

    a

--- engin/fixing.mjs
+++ engin/fixing.mjs
@@ -244,10 +244,10 @@ async function handleform(
   inn++;
 }
 async function fetchCPU() {
-  let pages = 10;
+  let pages = 11;
   let count = 15;
   let arr = [];
-  while (pages <= 10) {
+  while (pages <= 11) {
     const detail_driver = await new Builder()
       .forBrowser("chrome")
       .setChromeOptions(chromeOptions)
