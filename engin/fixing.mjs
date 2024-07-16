cf106cf4ff9f79768ba578ef69bf6c98cd84eb6d
sayjun0505 <sayjun0505@gmail.com>
2024-07-16 08:46:53 -0700

    a

--- engin/fixing.mjs
+++ engin/fixing.mjs
@@ -226,7 +226,7 @@ async function fetchCPU() {
     .forBrowser("chrome")
     .setChromeOptions(chromeOptions)
     .build();
-  let pages = 21;
+  let pages = 22;
   let count = 15;
   let arr = [];
   try {
