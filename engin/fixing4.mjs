d7037c61166a8fe5f71d3b21d81bc6eff4931857
sayjun0505 <sayjun0505@gmail.com>
2024-07-18 15:03:25 -0700

    a

--- engin/fixing4.mjs
+++ engin/fixing4.mjs
@@ -311,7 +311,7 @@ async function handleform(
   inn++;
 }
 async function fetchCPU4() {
-  let pages = 8;
+  let pages = 13;
   let count = 15;
   let arr = [];
   const detail_driver = await new Builder()
