2addd3f0ad7a17de5e1996a600d92747db4f0e02
sayjun0505 <sayjun0505@gmail.com>
2024-07-18 13:28:16 -0700

    a

--- actions/dboperation.mjs
+++ actions/dboperation.mjs
@@ -14,25 +14,20 @@ const operRouter = express.Router();
 operRouter.get("/api/alldata", async (req, res) => {
   try {
     const filterstring = req.query.filter;
-    let allData, count;
-
+    let allData,count;
+  
     if (filterstring) {
       const regex = new RegExp(filterstring, "i");
-      allData = await CPUList.aggregate([
-        { $match: { name: { $regex: regex } } },
-        { $sample: { size: 36 } }
-      ]);
+      allData = await CPUList.find({ name: { $regex: regex } }).limit(36);
       // allData = await CPUList.find({ name: { $regex: regex } }).limit(36);
-      count = await CPUList.find({ name: { $regex: regex } }).countDocuments(
-        {}
-      );
+      count = await CPUList.find({ name: { $regex: regex } }).countDocuments({});
     } else {
       allData = await CPUList.find({}).limit(36);
       count = await CPUList.countDocuments({});
     }
 
     const ret = { data: allData, count: count };
-
+    
     res.json(ret);
   } catch (error) {
     console.error("Error fetching all data from MongoDB:", error);
@@ -42,18 +37,18 @@ operRouter.get("/api/alldata", async (req, res) => {
 
 operRouter.get("/api/spec", async (req, res) => {
   try {
-    const id = req.query.id;
+    const id = req.query.id; 
     if (id) {
       const allData = await CPUList.findOne({ _id: id });
       const vendorData = await CPUVendorList.find({ cpuid: id });
       const cpunatData = await CPUNat.findOne({ cpuid: id });
-      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
+      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
       res.json(ret);
     } else {
       const allData = await CPUInfo.find({});
       const vendorData = await CPUVendor.find({});
       const cpunatData = await CPUNat.find({ cpuid: id });
-      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
+      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
       res.json(ret);
     }
   } catch (error) {
@@ -75,18 +70,18 @@ operRouter.get("/api/allmdata", async (req, res) => {
 
 operRouter.get("/api/mspec", async (req, res) => {
   try {
-    const id = req.query.id;
+    const id = req.query.id; 
     if (id) {
       const allData = await MboardList.findOne({ _id: id });
       const vendorData = await MboardvendorList.find({ mboardid: id });
       const cpunatData = await MboardNat.findOne({ mboardid: id });
-      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
+      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
       res.json(ret);
     } else {
       const allData = await MboardList.find({});
       const vendorData = await MboardvendorList.find({});
       const cpunatData = await MboardNat.find({ cpuid: id });
-      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
+      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
       res.json(ret);
     }
   } catch (error) {
@@ -94,5 +89,6 @@ operRouter.get("/api/mspec", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
 
 export default operRouter;
