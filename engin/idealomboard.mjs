acfe5500e6f91ddb59492d0b26ac8bd445b367e9
sayjun0505 <sayjun0505@gmail.com>
2024-07-23 01:11:24 -0700

    a

--- engin/idealomboard.mjs
+++ engin/idealomboard.mjs
@@ -370,7 +370,7 @@ async function handleform(
 
 async function fetchMboard() {
   let arr = [];
-  let pages = 25;
+  let pages = 26;
   let count = 15;
   while (true) {
     const detail_driver = await new Builder()
