4c0aafec5243b48bfb58e4690ff3d961fab59edd
sayjun0505 <sayjun0505@gmail.com>
2024-07-23 05:12:22 -0700

    a

--- engin/idealoram.mjs
+++ engin/idealoram.mjs
@@ -270,7 +270,7 @@ async function handleform(
 }
 async function fetchRam() {
   let arr = [];
-  let pages = 82;
+  let pages = 83;
   let count = 15;
   while (true) {
     const detail_driver = await new Builder()
