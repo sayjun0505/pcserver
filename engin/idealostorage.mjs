38b532acda5145c14b8396704a1439d1c79b2a24
sayjun0505 <sayjun0505@gmail.com>
2024-07-22 20:52:29 -0700

    a

--- engin/idealostorage.mjs
+++ engin/idealostorage.mjs
@@ -270,7 +270,7 @@ async function handleform(
 }
 async function fetchStorage() {
   let arr = [];
-  let pages = 29;
+  let pages = 30;
   let count = 15;
   while (true) {
     const detail_driver = await new Builder()
