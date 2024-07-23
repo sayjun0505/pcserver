5a480449ed78aebe56a38dbd00d73bc4c7680c8a
sayjun0505 <sayjun0505@gmail.com>
2024-07-22 18:42:56 -0700

    a

--- engin/idealogpu.mjs
+++ engin/idealogpu.mjs
@@ -370,7 +370,7 @@ async function handleform(
 
 async function fetchGPU() {
   let arr = [];
-  let pages = 5;
+  let pages = 22;
   let count = 15;
   while (true) {
     const detail_driver = await new Builder()
