4d66a7ba0378da287fb5c5cd956c19aefd5bc5a5
sayjun0505 <sayjun0505@gmail.com>
2024-07-23 04:41:57 -0700

    a

--- engin/idealocase.mjs
+++ engin/idealocase.mjs
@@ -284,7 +284,7 @@ async function handleform(
 
 async function fetchCase() {
   let arr = [];
-  let pages = 64;
+  let pages = 67;
   let count = 15;
   while (true) {
     const detail_driver = await new Builder()
--- engin/idealostorage.mjs
+++ engin/idealostorage.mjs
@@ -270,7 +270,7 @@ async function handleform(
 }
 async function fetchStorage() {
   let arr = [];
-  let pages = 42;
+  let pages = 43;
   let count = 15;
   while (true) {
     const detail_driver = await new Builder()
