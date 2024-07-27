e54ef8b736eee26ae5456f19104a9e7545497e2d
sayjun0505 <sayjun0505@gmail.com>
2024-07-27 15:57:50 -0800

    a

--- actions/dboperation.mjs
+++ actions/dboperation.mjs
@@ -31,20 +31,32 @@ const operRouter = express.Router();
 operRouter.get("/api/alldata", async (req, res) => {
   try {
     const filterstring = req.query.filter;
-    let allData,count;
-  
+    const curpage = req.query.curpage;
+    let allData, count;
+
     if (filterstring) {
       const regex = new RegExp(filterstring, "i");
       allData = await CPUList.find({ name: { $regex: regex } }).limit(36);
       // allData = await CPUList.find({ name: { $regex: regex } }).limit(36);
-      count = await CPUList.find({ name: { $regex: regex } }).countDocuments({});
+      count = await CPUList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
     } else {
       allData = await CPUList.find({}).limit(36);
       count = await CPUList.countDocuments({});
     }
+    if (curpage) {
+      const regex = new RegExp(filterstring, "i");
+      const skip = (curpage - 1) * 36;
+      allData = await CPUList.find({ name: { $regex: regex } })
+        .skip(skip) // Skip the calculated number of records
+        .limit(36);
+      count = await CPUList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
+    }
 
     const ret = { data: allData, count: count };
-    
     res.json(ret);
   } catch (error) {
     console.error("Error fetching all data from MongoDB:", error);
@@ -54,18 +66,18 @@ operRouter.get("/api/alldata", async (req, res) => {
 
 operRouter.get("/api/spec", async (req, res) => {
   try {
-    const id = req.query.id; 
+    const id = req.query.id;
     if (id) {
       const allData = await CPUList.findOne({ _id: id });
       const vendorData = await CPUVendorList.find({ cpuid: id });
       const cpunatData = await CPUNat.findOne({ cpuid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     } else {
       const allData = await CPUInfo.find({});
       const vendorData = await CPUVendor.find({});
       const cpunatData = await CPUNat.find({ cpuid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     }
   } catch (error) {
@@ -77,16 +89,29 @@ operRouter.get("/api/spec", async (req, res) => {
 operRouter.get("/api/allmdata", async (req, res) => {
   try {
     const filterstring = req.query.filter;
-    let allData,count;
-  
+    const curpage = req.query.curpage;
+    let allData, count;
+
     if (filterstring) {
       const regex = new RegExp(filterstring, "i");
       allData = await MboardList.find({ name: { $regex: regex } }).limit(36);
       // allData = await CPUList.find({ name: { $regex: regex } }).limit(36);
-      count = await MboardList.find({ name: { $regex: regex } }).countDocuments({});
+      count = await MboardList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
     } else {
       allData = await MboardList.find({}).limit(36);
       count = await MboardList.countDocuments({});
+    }
+    if (curpage) {
+      const regex = new RegExp(filterstring, "i");
+      const skip = (curpage - 1) * 36;
+      allData = await MboardList.find({ name: { $regex: regex } })
+        .skip(skip) // Skip the calculated number of records
+        .limit(36);
+      count = await MboardList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
     }
 
     const ret = { data: allData, count: count };
@@ -99,18 +124,18 @@ operRouter.get("/api/allmdata", async (req, res) => {
 
 operRouter.get("/api/mspec", async (req, res) => {
   try {
-    const id = req.query.id; 
+    const id = req.query.id;
     if (id) {
       const allData = await MboardList.findOne({ _id: id });
       const vendorData = await MboardVendorList.find({ mboardid: id });
       const cpunatData = await MboardNat.findOne({ mboardid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     } else {
       const allData = await MboardList.findOne({ _id: id });
       const vendorData = await MboardVendorList.find({ mboardid: id });
       const cpunatData = await MboardNat.findOne({ mboardid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     }
   } catch (error) {
@@ -119,24 +144,36 @@ operRouter.get("/api/mspec", async (req, res) => {
   }
 });
 
-
 operRouter.get("/api/allrdata", async (req, res) => {
   try {
     const filterstring = req.query.filter;
-    let allData,count;
-  
+    const curpage = req.query.curpage;
+    let allData, count;
+
     if (filterstring) {
       const regex = new RegExp(filterstring, "i");
       allData = await RamList.find({ name: { $regex: regex } }).limit(36);
       // allData = await CPUList.find({ name: { $regex: regex } }).limit(36);
-      count = await RamList.find({ name: { $regex: regex } }).countDocuments({});
+      count = await RamList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
     } else {
       allData = await RamList.find({}).limit(36);
       count = await RamList.countDocuments({});
     }
+    if (curpage) {
+      const regex = new RegExp(filterstring, "i");
+      const skip = (curpage - 1) * 36;
+      allData = await RamList.find({ name: { $regex: regex } })
+        .skip(skip) // Skip the calculated number of records
+        .limit(36);
+      count = await RamList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
+    }
 
     const ret = { data: allData, count: count };
-    
+
     res.json(ret);
   } catch (error) {
     console.error("Error fetching all data from MongoDB:", error);
@@ -146,18 +183,18 @@ operRouter.get("/api/allrdata", async (req, res) => {
 
 operRouter.get("/api/rspec", async (req, res) => {
   try {
-    const id = req.query.id; 
+    const id = req.query.id;
     if (id) {
       const allData = await RamList.findOne({ _id: id });
       const vendorData = await RamVendorList.find({ ramid: id });
       const cpunatData = await RamNat.findOne({ ramid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     } else {
       const allData = await RamList.findOne({ _id: id });
       const vendorData = await RamVendorList.find({ ramid: id });
       const cpunatData = await RamNat.findOne({ ramid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     }
   } catch (error) {
@@ -166,24 +203,36 @@ operRouter.get("/api/rspec", async (req, res) => {
   }
 });
 
-
 operRouter.get("/api/allsdata", async (req, res) => {
   try {
     const filterstring = req.query.filter;
-    let allData,count;
-  
+    const curpage = req.query.curpage;
+    let allData, count;
+
     if (filterstring) {
       const regex = new RegExp(filterstring, "i");
       allData = await StorageList.find({ name: { $regex: regex } }).limit(36);
       // allData = await CPUList.find({ name: { $regex: regex } }).limit(36);
-      count = await StorageList.find({ name: { $regex: regex } }).countDocuments({});
+      count = await StorageList.find({
+        name: { $regex: regex }
+      }).countDocuments({});
     } else {
       allData = await StorageList.find({}).limit(36);
       count = await StorageList.countDocuments({});
     }
+    if (curpage) {
+      const regex = new RegExp(filterstring, "i");
+      const skip = (curpage - 1) * 36;
+      allData = await StorageList.find({ name: { $regex: regex } })
+        .skip(skip) // Skip the calculated number of records
+        .limit(36);
+      count = await StorageList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
+    }
 
     const ret = { data: allData, count: count };
-    
+
     res.json(ret);
   } catch (error) {
     console.error("Error fetching all data from MongoDB:", error);
@@ -193,18 +242,18 @@ operRouter.get("/api/allsdata", async (req, res) => {
 
 operRouter.get("/api/sspec", async (req, res) => {
   try {
-    const id = req.query.id; 
+    const id = req.query.id;
     if (id) {
       const allData = await StorageList.findOne({ _id: id });
       const vendorData = await StorageVendorList.find({ storageid: id });
       const cpunatData = await StorageNat.findOne({ storageid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     } else {
       const allData = await StorageList.findOne({ _id: id });
       const vendorData = await StorageVendorList.find({ storageid: id });
       const cpunatData = await StorageNat.findOne({ storageid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     }
   } catch (error) {
@@ -213,24 +262,35 @@ operRouter.get("/api/sspec", async (req, res) => {
   }
 });
 
-
 operRouter.get("/api/allcasedata", async (req, res) => {
   try {
     const filterstring = req.query.filter;
-    let allData,count;
-  
+    const curpage = req.query.curpage;
+    let allData, count;
+
     if (filterstring) {
       const regex = new RegExp(filterstring, "i");
       allData = await CaseList.find({ name: { $regex: regex } }).limit(36);
       // allData = await CPUList.find({ name: { $regex: regex } }).limit(36);
-      count = await CaseList.find({ name: { $regex: regex } }).countDocuments({});
+      count = await CaseList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
     } else {
       allData = await CaseList.find({}).limit(36);
       count = await CaseList.countDocuments({});
     }
-
+    if (curpage) {
+      const regex = new RegExp(filterstring, "i");
+      const skip = (curpage - 1) * 36;
+      allData = await CaseList.find({ name: { $regex: regex } })
+        .skip(skip) // Skip the calculated number of records
+        .limit(36);
+      count = await CaseList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
+    }
     const ret = { data: allData, count: count };
-    
+
     res.json(ret);
   } catch (error) {
     console.error("Error fetching all data from MongoDB:", error);
@@ -240,18 +300,18 @@ operRouter.get("/api/allcasedata", async (req, res) => {
 
 operRouter.get("/api/casespec", async (req, res) => {
   try {
-    const id = req.query.id; 
+    const id = req.query.id;
     if (id) {
       const allData = await CaseList.findOne({ _id: id });
       const vendorData = await CaseVendorList.find({ caseid: id });
       const cpunatData = await CaseNat.findOne({ caseid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     } else {
       const allData = await CaseList.findOne({ _id: id });
       const vendorData = await CaseVendorList.find({ caseid: id });
       const cpunatData = await CaseNat.findOne({ caseid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     }
   } catch (error) {
@@ -263,20 +323,33 @@ operRouter.get("/api/casespec", async (req, res) => {
 operRouter.get("/api/allgpudata", async (req, res) => {
   try {
     const filterstring = req.query.filter;
-    let allData,count;
-  
+    const curpage = req.query.curpage;
+    let allData, count;
+
     if (filterstring) {
       const regex = new RegExp(filterstring, "i");
       allData = await GPUList.find({ name: { $regex: regex } }).limit(36);
       // allData = await CPUList.find({ name: { $regex: regex } }).limit(36);
-      count = await GPUList.find({ name: { $regex: regex } }).countDocuments({});
+      count = await GPUList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
     } else {
       allData = await GPUList.find({}).limit(36);
       count = await GPUList.countDocuments({});
     }
+    if (curpage) {
+      const regex = new RegExp(filterstring, "i");
+      const skip = (curpage - 1) * 36;
+      allData = await GPUList.find({ name: { $regex: regex } })
+        .skip(skip) // Skip the calculated number of records
+        .limit(36);
+      count = await GPUList.find({ name: { $regex: regex } }).countDocuments(
+        {}
+      );
+    }
 
     const ret = { data: allData, count: count };
-    
+
     res.json(ret);
   } catch (error) {
     console.error("Error fetching all data from MongoDB:", error);
@@ -286,18 +359,18 @@ operRouter.get("/api/allgpudata", async (req, res) => {
 
 operRouter.get("/api/gpuspec", async (req, res) => {
   try {
-    const id = req.query.id; 
+    const id = req.query.id;
     if (id) {
       const allData = await GPUList.findOne({ _id: id });
       const vendorData = await GPUVendorList.find({ gpuid: id });
       const cpunatData = await GPUNat.findOne({ gpuid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     } else {
       const allData = await GPUList.findOne({ _id: id });
       const vendorData = await GPUVendorList.find({ gpuid: id });
       const cpunatData = await GPUNat.findOne({ gpuid: id });
-      const ret = { data: allData, vendor: vendorData,nat:cpunatData };
+      const ret = { data: allData, vendor: vendorData, nat: cpunatData };
       res.json(ret);
     }
   } catch (error) {
@@ -305,8 +378,5 @@ operRouter.get("/api/gpuspec", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-
-
-
 
 export default operRouter;
